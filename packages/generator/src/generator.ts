d427e26f5acc4554dc96091b817914462ca14166
dangchinh25 <dangchinh2501@gmail.com>
2023-11-02 14:36:37 -0500

    Update test prisma model

--- packages/generator/src/generator.ts
+++ packages/generator/src/generator.ts
@@ -1,7 +1,9 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 /* eslint-disable @typescript-eslint/no-non-null-asserted-optional-chain */
 /* eslint-disable @typescript-eslint/no-var-requires */
-import { generatorHandler, GeneratorOptions } from '@prisma/generator-helper';
+import {
+    generatorHandler, GeneratorManifest, GeneratorOptions
+} from '@prisma/generator-helper';
 import { logger } from '@prisma/sdk';
 import path from 'path';
 import { GENERATOR_NAME } from './constants';
@@ -11,15 +13,16 @@ import { writeFileSafely } from './utils/writeFileSafely';
 const { version } = require( '../package.json' );
 
 generatorHandler( {
-    onManifest () {
+    onManifest (): GeneratorManifest {
         logger.info( `${ GENERATOR_NAME }:Registered` );
+
         return {
             version,
             defaultOutput: '../generated',
             prettyName: GENERATOR_NAME
         };
     },
-    onGenerate: async ( options: GeneratorOptions ) => {
+    onGenerate: async ( options: GeneratorOptions ): Promise<void> => {
         options.dmmf.datamodel.enums.forEach( async ( enumInfo ) => {
             const tsEnum = genEnum( enumInfo );
 
@@ -30,5 +33,16 @@ generatorHandler( {
 
             await writeFileSafely( writeLocation, tsEnum );
         } );
+
+        options.dmmf.datamodel.models.forEach( model => {
+            console.log( model );
+        } );
+
+        const writeLocation = path.join(
+            options.generator.output?.value!,
+            `ModelsGraph.ts`
+        );
+
+        await writeFileSafely( writeLocation, '123' );
     }
 } );
