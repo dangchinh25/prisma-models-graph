571d9aab5abdfa3872d6c0d1709beabb8790d10e
dangchinh25 <dangchinh2501@gmail.com>
2023-11-02 16:38:36 -0500

    Init core logic to parse field documentation

--- packages/generator/src/helpers/model.ts
+++ packages/generator/src/helpers/model.ts
@@ -5,31 +5,65 @@ export const parseDMMFModels = (
     models: DMMF.Model[]
 ): ParsedModels => {
     const parsedModels: ParsedModels = {};
+    const modelsMap: Map<DMMF.Model['name'], DMMF.Model> = new Map();
+    const attributesDbNameMap: Map<string, string> = new Map();
+
+    for ( const model of models ) {
+        modelsMap.set( model.name, model );
+        const dbName = model.dbName || model.name;
+
+        const parsedModel: ParsedModel = {
+            attributes: [],
+            relations: []
+        };
+
+        for ( const field of model.fields ) {
+            const attribute = field.dbName || field.name;
+            parsedModel.attributes.push( attribute );
+            attributesDbNameMap.set( `${ model.name }.${ field.name }`, attribute );
+        }
+
+        parsedModels[ dbName ] = parsedModel;
+    }
 
     for ( const model of models ) {
         const dbName = model.dbName || model.name;
-        parsedModels[ dbName ] = parseDMMFModel( model );
+
+        for ( const field of model.fields ) {
+            const { documentation } = field;
+            const fieldDbName = field.dbName || field.name;
+
+            if ( !documentation ) {
+                continue;
+            }
+
+            const relationAttribute = documentation.replace( /\[\[|\]\]/g, '' );
+            const relationModelName = relationAttribute.split( '.' )[ 0 ];
+            const relationAttributeDbName = attributesDbNameMap.get( relationAttribute );
+
+            if ( !relationAttributeDbName ) {
+                continue;
+            }
+
+            const relationModel = modelsMap.get( relationModelName );
+
+            if ( !relationModel ) {
+                continue;
+            }
+
+            const relationModelDbName = relationModel.dbName || relationModel.name;
+
+            parsedModels[ dbName ].relations.push( {
+                modelName: relationModelDbName,
+                condition: `${ dbName }.${ fieldDbName } = ${ relationModelDbName }.${ relationAttributeDbName }`
+            } );
+
+            parsedModels[ relationModelDbName ].relations.push( {
+                modelName: dbName,
+                condition: `${ relationModelDbName }.${ relationAttributeDbName } = ${ dbName }.${ fieldDbName }`
+            } );
+        }
     }
 
     return parsedModels;
-};
-
-export const parseDMMFModel = (
-    model: DMMF.Model
-): ParsedModel => {
-    console.log( model );
-
-    const parsedModel: ParsedModel = {
-        attributes: [],
-        relations: []
-    };
-
-    const { fields } = model;
-
-    for ( const field of fields ) {
-        const attribute = field.dbName || field.name;
-        parsedModel.attributes.push( attribute );
-    }
-
-    return parsedModel;
 };
