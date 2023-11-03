3795670d421b3edea475789d92eb931873ad44e5
dangchinh25 <dangchinh2501@gmail.com>
2023-11-03 09:38:14 -0500

    fix: prettier config not found

--- packages/generator/src/utils/formatFile.ts
+++ packages/generator/src/utils/formatFile.ts
@@ -5,26 +5,11 @@ import prettier, { Options } from 'prettier';
  * @param content
  * @param parser Prettier parser. Find out more [here](https://prettier.io/docs/en/options.html#parser). Default is 'json-stringify'
  */
-export const formatFile = (
+export const formatFile = async (
     content: string,
     parser: Options['parser'] = 'json-stringify'
 ): Promise<string> => {
-    return new Promise( ( res, rej ) =>
-        prettier.resolveConfig( process.cwd() )
-            .then( ( options ) => {
-                if ( !options ) {
-                    res( content ); // no prettier config was found, no need to format
-                }
+    const formatted = prettier.format( content, { parser } );
 
-                try {
-                    const formatted = prettier.format( content, {
-                        ...options,
-                        parser
-                    } );
-
-                    res( formatted );
-                } catch ( error ) {
-                    rej( error );
-                }
-            } ) );
+    return formatted;
 };
