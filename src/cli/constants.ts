51fea31e29b600f45b83149adb72d734907ff832
dangchinh25 <dangchinh2501@gmail.com>
2023-11-06 09:39:23 -0600

    feat: option to export json models graph

--- src/cli/constants.ts
+++ src/cli/constants.ts
@@ -1,5 +1,5 @@
 export const GENERATOR_NAME = 'prisma-models-graph';
 
-export const DEFAULT_FILE_NAME = 'ModelsGraph.json';
+export const DEFAULT_JSON_FILE_NAME = 'ModelsGraph.json';
 
 export const DEFAULT_OUTPUT_JSON_FOLDER = './modelsGraph';
--- src/cli/prisma-generator.ts
+++ src/cli/prisma-generator.ts
@@ -1,9 +1,25 @@
+/* eslint-disable @typescript-eslint/no-non-null-asserted-optional-chain */
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { GeneratorOptions } from '@prisma/generator-helper';
 import { generateCode } from '../generator/generateCode';
 import { parseDMMFModels } from '../generator/helpers';
+import path from 'path';
+import { DEFAULT_JSON_FILE_NAME } from './constants';
+import { writeFileSafely } from '../utils';
 
 export const generate = async ( options: GeneratorOptions ): Promise<void> => {
     const modelsGraph = parseDMMFModels( options.dmmf.datamodel.models );
 
-    await Promise.all( [ generateCode( modelsGraph ) ] );
+    const { exportJSON } = options.generator.config;
+
+    if ( exportJSON === 'true' ) {
+        const jsonWriteLocation = path.join(
+            options.generator.output?.value!,
+            DEFAULT_JSON_FILE_NAME
+        );
+
+        await writeFileSafely( jsonWriteLocation, JSON.stringify( modelsGraph ) );
+    }
+
+    await generateCode( modelsGraph );
 };
